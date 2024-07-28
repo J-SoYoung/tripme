3e0e63393c19ef41b1ec7aa9884e103da1288d89
thdud <thgml1444@naver.com>
2024-07-28 13:43:25 +0900

    Add : created bottomNavbar in layout Component and used common to app pages - 레이아웃 컴포넌트에 클라이언트/서버 컴포넌트의 기능이 둘다 필요해서 두 기능을 분리하여 컴포넌트 생성 -> ClientLayout - BottomNavBar => home, search, wish, mypage 페이지 연결 - global.css : body는 배경색을 넣고, layout컴포넌트의 app 클래스에는 해당 프로젝트의 너비와 공통 스타일 적용

---
+++ src/app/components/common/BottomNavBar.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+import Home from "../../../../public/assets/icons/nav_home.svg";
+import Mypage from "../../../../public/assets/icons/nav_mypage.svg";
+import Search from "../../../../public/assets/icons/nav_search.svg";
+import Wish from "../../../../public/assets/icons/nav_wish.svg";
+
+export default function BottomNavBar() {
+  return (
+    <nav className="fixed bottom-0 mx-auto w-[499px] max-w-[600px] border-t-2 bg-[#fff]">
+      <ul className="flex justify-around p-4">
+        <Link href="/home" className="text-center cursor-pointer">
+          <Image
+            width={30}
+            height={30}
+            src={Home}
+            alt="Home"
+            className="w-6 h-6 mx-auto"
+          />
+          <span className="text-xs">HOME</span>
+        </Link>
+        <Link href="/search" className="text-center cursor-pointer">
+          <Image
+            width={30}
+            height={30}
+            src={Search}
+            alt="SEARCH"
+            className="w-6 h-6 mx-auto"
+          />
+          <span className="text-xs">SEARCH</span>
+        </Link>
+        <Link href="/wish" className="text-center cursor-pointer">
+          <Image
+            width={30}
+            height={30}
+            src={Wish}
+            alt="Wish"
+            className="w-6 h-6 mx-auto"
+          />
+          <span className="text-xs">WISH</span>
+        </Link>
+        <Link href="/mypage" className="text-center cursor-pointer">
+          <Image
+            width={30}
+            height={30}
+            src={Mypage}
+            alt="Mypage"
+            className="w-6 h-6 mx-auto"
+          />
+          <span className="text-xs">MYPAGE</span>
+        </Link>
+      </ul>
+    </nav>
+  );
+}
